9194df736d365d9d00e5c5c76a21b5dc74d8a1d8
Ulysse Carion <ulysse@ulysse.io>
2017-10-15 16:42:40 -0700

    Generate geojson-related data (global and per-tile)

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,24 @@ extern crate error_chain;
 #[macro_use]
 extern crate serde_derive;
 
+extern crate colored;
+extern crate geo;
+extern crate geojson;
 extern crate serde;
 extern crate serde_json;
 
-extern crate colored;
-
+use std::collections::BTreeMap;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 use std::str::FromStr;
 
 use colored::Colorize;
+use geojson::GeoJson;
+use geojson::conversion::TryInto;
+use geo::boundingbox::BoundingBox;
+use geo::simplify::Simplify;
 
 mod errors {
     error_chain!{}
@@ -24,7 +30,8 @@ use errors::*;
 
 pub const ELEVATION_OFFSET: u16 = 500;
 
-const NUM_STEPS: u32 = 4;
+// This is just for logging purposes
+const NUM_STEPS: u32 = 6;
 
 const MAX_LEVEL: u32 = 6;
 const ELEVATION_TILE_SIZE: u32 = 64;
@@ -46,11 +53,30 @@ struct FirstPassTileMetadata {
     max_elevation: u16,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TileMetadata {
+    pub min_elevation: u16,
+    pub max_elevation: u16,
+    pub polygons: Vec<u64>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PolygonPointData {
+    pub polygons: Vec<MultiLevelPolygon>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MultiLevelPolygon {
+    pub properties: serde_json::Map<String, serde_json::Value>,
+    pub levels: Vec<Vec<(f32, f32)>>,
+}
+
 #[derive(Debug)]
 pub struct PrepareAssetsTask {
     noaa_globe_dir: PathBuf,
     nasa_blue_marble_dir: PathBuf,
     features_file: PathBuf,
+    simplification_epsilons: [f32; MAX_LEVEL as usize + 1],
     output_dir: PathBuf,
 }
 
@@ -60,6 +86,7 @@ impl PrepareAssetsTask {
             noaa_globe_dir: "".into(),
             nasa_blue_marble_dir: "".into(),
             features_file: "".into(),
+            simplification_epsilons: [0.0; MAX_LEVEL as usize + 1],
             output_dir: "".into(),
         }
     }
@@ -92,6 +119,16 @@ impl PrepareAssetsTask {
         }
     }
 
+    pub fn with_simplification_epsilons(
+        self,
+        simplification_epsilons: [f32; MAX_LEVEL as usize + 1],
+    ) -> PrepareAssetsTask {
+        PrepareAssetsTask {
+            simplification_epsilons: simplification_epsilons,
+            ..self
+        }
+    }
+
     pub fn run(&self) -> Result<()> {
         if !self.crops_dir().is_dir() {
             std::fs::create_dir(&self.crops_dir()).chain_err(
@@ -115,6 +152,8 @@ impl PrepareAssetsTask {
         for level in 0..MAX_LEVEL + 1 {
             self.create_tile_level(level)?;
         }
+
+        self.create_polygon_data()?;
 
         Ok(())
     }
@@ -379,6 +418,13 @@ impl PrepareAssetsTask {
     }
 
     fn create_tile_level(&self, level: u32) -> Result<()> {
+        if self.tiles_dir()
+            .join(format!("{}_0_0.elevation", level))
+            .is_file()
+        {
+            return Ok(());
+        }
+
         self.pretty_log(
             4,
             level + 1,
@@ -496,11 +542,14 @@ impl PrepareAssetsTask {
                 let min = (min * u16::max_value() as f32) as u16;
                 let max = (max * u16::max_value() as f32) as u16;
 
-                let metadata_path = self.tiles_dir().join(
-                    format!("{}_{}_{}.json", level, tile_x, tile_y),
-                );
+                let metadata_path = self.tiles_dir().join(format!(
+                    "{}_{}_{}.json",
+                    level,
+                    tile_x,
+                    tile_y
+                ));
                 let mut metadata_file = File::create(metadata_path).chain_err(
-                    || "Unable to create tile metadata file",
+                    || "Unable to create first pass tile metadata file",
                 )?;
 
                 let metadata = FirstPassTileMetadata {
@@ -510,10 +559,179 @@ impl PrepareAssetsTask {
 
                 let to_write = serde_json::to_string(&metadata).unwrap();
                 metadata_file.write_all(to_write.as_bytes()).chain_err(
-                    || "Error writing out tile metadata",
+                    || "Error writing out first pass tile metadata",
                 )?;
             }
         }
+
+        Ok(())
+    }
+
+    fn create_polygon_data(&self) -> Result<()> {
+        // if self.output_dir.join("polygons.json").is_file() {
+        //     return Ok(())
+        // }
+
+        let mut features_file = File::open(&self.features_file).chain_err(
+            || "Could not open features file",
+        )?;
+        let mut geojson = String::new();
+        features_file.read_to_string(&mut geojson).chain_err(
+            || "Could not read from features file",
+        )?;
+
+        let geojson: GeoJson = geojson.parse().unwrap();
+        let feature_collection = match geojson {
+            GeoJson::FeatureCollection(fc) => fc,
+            _ => panic!("Unexpected geojson object type!"),
+        };
+
+        let mut polygons = Vec::new();
+        let mut polygon_properties = Vec::new();
+
+        for feature in feature_collection.features {
+            let properties = feature.properties.unwrap_or(serde_json::Map::new());
+            let geometry: geo::Geometry<f32> = feature.geometry.unwrap().value.try_into().unwrap();
+
+            match geometry {
+                geo::Geometry::MultiPolygon(mp) => {
+                    for polygon in mp {
+                        polygons.push(polygon);
+                        polygon_properties.push(properties.clone());
+                    }
+                }
+                geo::Geometry::Polygon(p) => {
+                    polygons.push(p);
+                    polygon_properties.push(properties);
+                }
+                _ => panic!(),
+            }
+        }
+
+        self.create_final_tile_metadatas(&polygons)?;
+        self.create_polygon_file(&polygons, &polygon_properties)?;
+        Ok(())
+    }
+
+    fn create_final_tile_metadatas(&self, polygons: &[geo::Polygon<f32>]) -> Result<()> {
+        let mut tile_metadatas: BTreeMap<(u32, u32, u32), Vec<u64>> = BTreeMap::new();
+        self.pretty_log(5, 1, 1, "Generating tile metadata files");
+
+        for level in 0..MAX_LEVEL + 1 {
+            let num_tiles_across_width = TILES_ACROSS_WIDTH / 2u32.pow(level);
+            let num_tiles_across_height = num_tiles_across_width / 2;
+
+            for (polygon_index, polygon) in polygons.iter().enumerate() {
+                let polygon_bbox = polygon.exterior.bbox().unwrap();
+                let x_min = (polygon_bbox.xmin + 180.0) / 360.0;
+                let x_max = (polygon_bbox.xmax + 180.0) / 360.0;
+                let y_min = (-polygon_bbox.ymin + 90.0) / 180.0;
+                let y_max = (-polygon_bbox.ymax + 90.0) / 180.0;
+
+                let xs = (x_min * num_tiles_across_width as f32) as u32..
+                    (x_max * num_tiles_across_width as f32) as u32 + 1;
+                let ys = (y_max * num_tiles_across_height as f32) as u32..
+                    (y_min * num_tiles_across_height as f32) as u32 + 1;
+
+                for x in xs {
+                    for y in ys.clone() {
+                        tile_metadatas
+                            .entry((level, x, y))
+                            .or_insert(Vec::new())
+                            .push(polygon_index as u64);
+                    }
+                }
+            }
+        }
+
+        for level in 0..MAX_LEVEL + 1 {
+            let num_tiles_across_width = TILES_ACROSS_WIDTH / 2u32.pow(level);
+            let num_tiles_across_height = num_tiles_across_width / 2;
+
+            for x in 0..num_tiles_across_width {
+                for y in 0..num_tiles_across_height {
+                    let metadata_path =
+                        self.tiles_dir().join(format!("{}_{}_{}.json", level, x, y));
+                    let metadata_file = File::open(&metadata_path).chain_err(
+                        || "Unable to read in first pass metadata",
+                    )?;
+                    let first_pass_metadata: FirstPassTileMetadata =
+                        serde_json::from_reader(metadata_file).chain_err(
+                            || "Error parsing first pass metadata",
+                        )?;
+
+                    let default_polygons = Vec::new();
+                    let polygons = tile_metadatas.get(&(level, x, y)).unwrap_or(
+                        &default_polygons,
+                    );
+
+                    let tile_metadata = TileMetadata {
+                        min_elevation: first_pass_metadata.min_elevation,
+                        max_elevation: first_pass_metadata.max_elevation,
+                        polygons: polygons.clone(),
+                    };
+
+                    let mut metadata_file = File::create(metadata_path).chain_err(
+                        || "Unable to open metadata file",
+                    )?;
+
+                    let to_write = serde_json::to_string(&tile_metadata).unwrap();
+                    metadata_file.write_all(to_write.as_bytes()).chain_err(
+                        || "Error writing out tile metadata",
+                    )?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn create_polygon_file(
+        &self,
+        polygons: &[geo::Polygon<f32>],
+        polygon_properties: &[serde_json::Map<String, serde_json::Value>],
+    ) -> Result<()> {
+        self.pretty_log(6, 1, 1, "Generating polygon point data file");
+
+        let multi_level_polygons = polygons
+            .iter()
+            .zip(polygon_properties)
+            .map(|(polygon, properties)| {
+                let levels = (0..MAX_LEVEL + 1)
+                    .map(|level| {
+                        let polygon =
+                            polygon.simplify(&self.simplification_epsilons[level as usize]);
+                        let mut points = Vec::new();
+
+                        points.extend(polygon.exterior.into_iter().map(
+                            |point| (point.x(), point.y()),
+                        ));
+                        for ring in polygon.interiors {
+                            points.extend(ring.into_iter().map(|point| (point.x(), point.y())));
+                        }
+
+                        points
+                    })
+                    .collect();
+
+                MultiLevelPolygon {
+                    properties: properties.clone(),
+                    levels: levels,
+                }
+            })
+            .collect();
+
+        let polygon_path = self.output_dir.join("polygons.json");
+        let mut polygon_file = File::create(polygon_path).chain_err(
+            || "Unable to open polygons file",
+        )?;
+
+        let polygon_point_data = PolygonPointData { polygons: multi_level_polygons };
+
+        let to_write = serde_json::to_string(&polygon_point_data).unwrap();
+        polygon_file.write_all(to_write.as_bytes()).chain_err(
+            || "Error writing out polygon point data",
+        )?;
 
         Ok(())
     }
